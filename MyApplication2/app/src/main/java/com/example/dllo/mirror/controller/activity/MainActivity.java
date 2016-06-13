3801f141c353e2bd5479e076adf2088985acb7fa
张锡威 <chshkfb@163.com>
2016-06-13 19:30:19 +0800

    no message

--- MyApplication2/app/src/main/java/com/example/dllo/mirror/controller/activity/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/dllo/mirror/controller/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.dllo.mirror.model.activity;
+package com.example.dllo.mirror.controller.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
