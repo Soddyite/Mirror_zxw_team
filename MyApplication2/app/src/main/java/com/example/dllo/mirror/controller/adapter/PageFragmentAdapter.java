6fc4e236ea4b476eb1e95e83d264d46c83c2c83c
hongxiang <916634913@qq.com>
2016-06-29 19:43:08 +0800

    6.29

--- MyApplication2/app/src/main/java/com/example/dllo/mirror/controller/adapter/PageFragmentAdapter.java
+++ MyApplication2/app/src/main/java/com/example/dllo/mirror/controller/adapter/PageFragmentAdapter.java
@@ -3,21 +3,26 @@ package com.example.dllo.mirror.controller.adapter;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.media.Image;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.dllo.mirror.R;
+import com.example.dllo.mirror.model.bean.GoodsDetails;
 import com.example.dllo.mirror.model.myinterface.PageItemClickListener;
-import com.example.dllo.mirror.model.utils.OkHttpClientManager;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
-import com.squareup.picasso.Picasso;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
 
-import java.io.IOException;
+import java.util.List;
+
+import it.sephiroth.android.library.picasso.Picasso;
 
 /**
  * Created by dllo on 16/6/16.
@@ -25,6 +30,15 @@ import java.io.IOException;
 public class PageFragmentAdapter extends RecyclerView.Adapter<PageFragmentAdapter.MyViewHolder> {
     private Context context;
     PageItemClickListener pageItemClickListener;
+    private List<GoodsDetails.DataBean.ListBean> list;
+    private String goods_img, brand, goods_price, product_area, story_url, story_title;
+    private String goods_name;
+
+
+    public void setList(List<GoodsDetails.DataBean.ListBean> list) {
+        this.list = list;
+        notifyDataSetChanged();
+    }
 
     public PageFragmentAdapter(Context context) {
         this.context = context;
@@ -38,21 +52,52 @@ public class PageFragmentAdapter extends RecyclerView.Adapter<PageFragmentAdapte
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.item_mainrecycle, parent, false);
         MyViewHolder myviewHolder = new MyViewHolder(view);
+
         return myviewHolder;
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, final int position) {
-
-
         DisplayImageOptions options;
         options = new DisplayImageOptions.Builder()
-                .cacheInMemory(true)//设置下载的图片是否缓存在内存中
-                .cacheOnDisk(true)//设置下载的图片是否缓存在SD卡中
-                .bitmapConfig(Bitmap.Config.RGB_565)//设置图片的解码类型//
+                .cacheOnDisk(true) //设置下载的图片是否缓存在SD卡中
+                .considerExifParams(true) //是否考虑JPEG图像EXIF参数（旋转，翻转）
+                .imageScaleType(ImageScaleType.IN_SAMPLE_INT) //设置图片以如何的编码方式显示
+                .bitmapConfig(Bitmap.Config.RGB_565) //设置图片的解码类型//
+                //.decodingOptions(android.graphics.BitmapFactory.Options decodingOptions) //设置图片的解码配置
+                .delayBeforeLoading(0) //int delayInMillis为你设置的下载前的延迟时间
+                .resetViewBeforeLoading(false)//设置图片在下载前是否重置，复位
+                .displayer(new FadeInBitmapDisplayer(100))//是否图片加载好后渐入的动画时间
+
                 .build();//构建完成
-        ImageLoader.getInstance().displayImage("http://cdn.duitang.com/uploads/item/201511/21/20151121222701_Qym8J.jpeg", holder.imageView,options);
-        if (holder!=null){
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        int type = Integer.parseInt(list.get(position).getType());
+        goods_img = list.get(position).getData_info().getGoods_img();
+        brand = list.get(position).getData_info().getBrand();
+        story_url = list.get(position).getData_info().getStory_img();
+        story_title = list.get(position).getData_info().getStory_title();
+        goods_price = list.get(position).getData_info().getGoods_price();
+        product_area = list.get(position).getData_info().getProduct_area();
+        goods_name = list.get(position).getData_info().getGoods_name();
+        holder.goods_name.setText(goods_name);
+        holder.goods_price.setText(goods_price);
+        holder.product_area.setText(product_area);
+        if (type == 2) {
+            holder.goods_name.setVisibility(View.GONE);
+            holder.goods_price.setVisibility(View.GONE);
+            holder.product_area.setVisibility(View.GONE);
+            holder.brand.setText(story_title);
+            ImageLoader.getInstance().displayImage(story_url, holder.imageView, options);
+            //Picasso.with(context).load(story_url).fit().into(holder.imageView);
+        } else {
+            holder.brand.setText(brand);
+            imageLoader.displayImage(goods_img, holder.imageView, options);
+
+            // Picasso.with(context).load(goods_img).fit().into(holder.imageView);
+
+
+        }
+        if (holder != null) {
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -66,15 +111,21 @@ public class PageFragmentAdapter extends RecyclerView.Adapter<PageFragmentAdapte
 
     @Override
     public int getItemCount() {
-        return 20;
+        return 10;
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
+        private TextView goods_price, goods_name, product_area, brand;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             imageView = (ImageView) itemView.findViewById(R.id.item_image);
+            goods_price = (TextView) itemView.findViewById(R.id.item_glass_goods_price);
+            product_area = (TextView) itemView.findViewById(R.id.item_glass_product_area);
+            brand = (TextView) itemView.findViewById(R.id.item_glass_brand);
+            goods_name = (TextView) itemView.findViewById(R.id.item_goods_name);
+
         }
     }
 }
--- MyApplication2/app/src/main/java/com/example/dllo/mirror/model/base/MyApplication.java
+++ MyApplication2/app/src/main/java/com/example/dllo/mirror/model/base/MyApplication.java
@@ -11,6 +11,7 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
+import com.nostra13.universalimageloader.utils.StorageUtils;
 
 import java.io.File;
 
@@ -24,11 +25,34 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
-        ImageLoaderConfiguration imageLoaderConfiguration = ImageLoaderConfiguration.createDefault(this);
-        ImageLoader.getInstance().init(imageLoaderConfiguration);
+        aaa(context);
     }
 
+    private void aaa(Context context) {
+        File cacheDir = StorageUtils.getOwnCacheDirectory(context, "universalimageloader/Cache");
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+                .memoryCacheExtraOptions(480, 800) // max width, max height，即保存的每个缓存文件的最大长宽
+                .threadPoolSize(3) //线程池内线程的数量
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                .denyCacheImageMultipleSizesInMemory()
+                .diskCacheFileNameGenerator(new Md5FileNameGenerator()) //将保存的时候的URI名称用MD5 加密
+                .memoryCache(new UsingFreqLimitedMemoryCache(2 * 1024 * 1024))
+                .memoryCacheSize(2 * 1024 * 1024) // 内存缓存的最大值
+                .diskCacheSize(50 * 1024 * 1024)  // SD卡缓存的最大值
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                // 由原先的discCache -> diskCache
+                .diskCache(new UnlimitedDiskCache(cacheDir))//自定义缓存路径
+                .imageDownloader(new BaseImageDownloader(context, 5 * 1000, 30 * 1000)) // connectTimeout (5 s), readTimeout (30 s)超时时间
+                .writeDebugLogs() // Remove for release app
+                .build();
+        //全局初始化此配置
+        ImageLoader.getInstance().init(config);
+    }
+
+
     public static Context getContext() {
+
+
         return context;
     }
 }
